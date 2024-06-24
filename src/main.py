abf40b6c87a18db4766d21c113dcf82e1fd1cc5b
OLODiman11 <OLODiman11@mail.ru>
2024-06-24 10:48:37 +0300

    Basic join logic

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello World!"
+    if request.method == 'POST':
+        return redirect(url_for('room'))
+    return render_template('index.html')
+
+
+@app.route('/room')
+def room():
+    return render_template('room.html')
 
 
 if __name__ == '__main__':
