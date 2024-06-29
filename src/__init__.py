efc5d0c13d108386c1a8f770ce0267ffaed0cec4
OLODiman11 <OLODiman11@mail.ru>
2024-06-29 18:17:33 +0300

    Added sqlite

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -22,5 +24,7 @@ def create_app(test_config=None):
     @app.route('/test')
     def test():
         return 'Success!'
-    
+
+    db.init_app(app)
+
     return app
